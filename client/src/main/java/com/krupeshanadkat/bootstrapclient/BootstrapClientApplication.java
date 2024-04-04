df40b5a31142a1d4d8ec1fa2309e0fb4cf6584e8
Krupesh Anadkat <kmanadkat@gmail.com>
2024-04-04 16:22:41 +0530

    feat: Project Restructure & Client Boilerplate

---
+++ client/src/main/java/com/krupeshanadkat/bootstrapclient/BootstrapClientApplication.java
@@ -0,0 +1,13 @@
+package com.krupeshanadkat.bootstrapclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapClientApplication.class, args);
+	}
+
+}
---
+++ client/src/test/java/com/krupeshanadkat/bootstrapclient/BootstrapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krupeshanadkat.bootstrapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
