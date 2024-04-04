a5a5f35e73cd9bbdbcde815cec892dd4a340fe09
Krupesh Anadkat <kmanadkat@gmail.com>
2024-04-04 16:02:17 +0530

    feat: Added Eureka - Services Registry Server

---
+++ src/main/java/com/krupeshanadkat/bootstrap/BootstrapApplication.java
@@ -0,0 +1,15 @@
+package com.krupeshanadkat.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapApplication.class, args);
+	}
+
+}
